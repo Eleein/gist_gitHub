6c184a2f8bdc9319a3c4a4235844175fcc21420f
dania <deduany@gmail.com>
2024-01-11 21:12:29 +0100

    refactor gist.cy.js
    Added describe for create and list tests.
    Moved partialUrl to top of the describe function
    Added start script to package.json

--- cypress/e2e/gist.cy.js
+++ cypress/e2e/gist.cy.js
@@ -5,64 +5,74 @@ describe("Gist Github", () => {
     cy.loginToGithub();
     cy.visit("/");
   });
+  describe("Gist Github: Create Gist", () => {
+    it("should create a secret gist by typing in editor, and should then delete the gist", () => {
+      const description = "Cypress test";
+      const text = "This is an automatic test";
+      const fileName = "test.txt";
 
-  it("should create a secret gist by typing in editor, and should then delete the gist", () => {
-    const description = "Cypress test";
-    const text = "This is an automatic test";
-    const fileName = "test.txt";
+      cy.get('input[aria-label="Gist description"]').type(description);
+      cy.get('input[aria-label="Filename including extension…"]').type(
+        fileName,
+      );
+      cy.get("#code-editor").type(text);
+      cy.contains("button", "Create secret gist").click();
 
-    cy.get('input[aria-label="Gist description"]').type(description);
-    cy.get('input[aria-label="Filename including extension…"]').type(fileName);
-    cy.get("#code-editor").type(text);
-    cy.contains("button", "Create secret gist").click();
+      cy.url().should("include", partialUrl);
+      cy.contains("a", fileName)
+        .should("be.visible")
+        .and("have.attr", "href")
+        .and("includes", partialUrl);
+      cy.contains(
+        'span[title="Only those with the link can see this gist."]',
+        "Secret",
+      ).should("be.visible");
+      cy.get("div #file-test-txt").should("be.visible");
 
-    cy.url().should("include", partialUrl);
-    cy.contains("a", fileName)
-      .should("be.visible")
-      .and("have.attr", "href")
-      .and("includes", partialUrl);
-    cy.contains(
-      'span[title="Only those with the link can see this gist."]',
-      "Secret",
-    ).should("be.visible");
-    cy.get("div #file-test-txt").should("be.visible");
-
-    cy.get('.pagehead-actions button[aria-label="Delete this Gist"]').click();
-  });
-  it("should create a secret gist by drag and drop in editor, and should then delete Gist", () => {
-    const description = "Another Cypress test";
-    const fileName = "people.csv";
-
-    cy.get('input[aria-label="Gist description"]').type(description);
-    cy.get('input[aria-label="Filename including extension…"]').type(fileName);
-    cy.get("#code-editor").selectFile("cypress/fixtures/people.csv", {
-      action: "drag-drop",
+      cy.get('.pagehead-actions button[aria-label="Delete this Gist"]').click();
     });
-    cy.contains("button", "Create secret gist").click();
+    it("should create a secret gist by drag and drop in editor, and should then delete Gist", () => {
+      const description = "Another Cypress test";
+      const fileName = "people.csv";
 
-    cy.url().should("include", partialUrl);
-    cy.contains("a", fileName)
-      .should("be.visible")
-      .and("have.attr", "href")
-      .and("includes", partialUrl);
-    cy.contains(
-      'span[title="Only those with the link can see this gist."]',
-      "Secret",
-    ).should("be.visible");
-    cy.get("div #file-people-csv").should("be.visible");
+      cy.get('input[aria-label="Gist description"]').type(description);
+      cy.get('input[aria-label="Filename including extension…"]').type(
+        fileName,
+      );
+      cy.get("#code-editor").selectFile("cypress/fixtures/people.csv", {
+        action: "drag-drop",
+      });
+      cy.contains("button", "Create secret gist").click();
 
-    cy.get('.pagehead-actions button[aria-label="Delete this Gist"]').click();
+      cy.url().should("include", partialUrl);
+      cy.contains("a", fileName)
+        .should("be.visible")
+        .and("have.attr", "href")
+        .and("includes", partialUrl);
+      cy.contains(
+        'span[title="Only those with the link can see this gist."]',
+        "Secret",
+      ).should("be.visible");
+      cy.get("div #file-people-csv").should("be.visible");
+
+      cy.get('.pagehead-actions button[aria-label="Delete this Gist"]').click();
+    });
   });
-  it("should list your gists", () => {
-    const baseUrl = `https://gist.github.com/${Cypress.env("githubUsername")}`;
 
-    cy.get('summary[aria-label="View profile and more"]')
-      .find("span.dropdown-caret")
-      .click();
-    cy.contains("a", "Your gists").click();
+  describe("Gist GitHub: List Gists", () => {
+    it("should list your gists", () => {
+      const baseUrl = `https://gist.github.com/${Cypress.env(
+        "githubUsername",
+      )}`;
 
-    cy.url().should("eq", baseUrl);
+      cy.get('summary[aria-label="View profile and more"]')
+        .find("span.dropdown-caret")
+        .click();
+      cy.contains("a", "Your gists").click();
 
-    cy.contains("a.UnderlineNav-item span", "All gists").should("be.visible");
+      cy.url().should("eq", baseUrl);
+
+      cy.contains("a.UnderlineNav-item span", "All gists").should("be.visible");
+    });
   });
 });
